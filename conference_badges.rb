f9a89b4822ac483bc02037d6b0b6e4a0d5bdcefa
Kayla Cross <kac288@georgetown.edu>
2018-02-17 16:39:08 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,6 +8,12 @@ def batch_badge_creator(array)
   array.collect {|name| "Hello, my name is #{name}."}
 end
 
+def assign_rooms(array)
+  array.collect.with_index do |name| 
+    "Hello, my name is #{name}."
+  end
+end
+
 test_array = ["Kayla","Nathan","Melanie","Mark"]
 
 print batch_badge_creator(test_array)
